e8b45d975d9fe2c968293bab825cc6fe9ef51a0c
Tane10 <16818483+Tane10@users.noreply.github.com>
2020-09-27 19:19:32 +0100

    we have the trie search working on our wordlist, and we can check is the word is a real word. but we seem to limited. words like home, cat, jam don't show up as the predicted words

--- node_api/src/services/word_logic_service.js
+++ node_api/src/services/word_logic_service.js
@@ -1,9 +1,21 @@
-class TransformStringLogic {
+const fs = require('fs')
+
+class TrieNode {
+    constructor() {
+        this.isEnd = false
+        this.children = {}
+    }
+}
+
+
+class TrieTransformStringLogic {
     constructor(rawNumberString) {
         this.rawNumberString = rawNumberString;
+        this.root = new TrieNode();
     }
 
     _letterMatrix = [];
+    _wordList = JSON.parse(fs.readFileSync('wordlist.json'));
 
     _numberLetterMappings = {
         "2": ["a", "b", "c"],
@@ -15,9 +27,12 @@ class TransformStringLogic {
         "8": ["t", "u", "v"],
         "9": ["w", "x", "y", "z"],
         "0": [" "]
-    }
+    };
 
     letterCombination() {
+        this._wordList.forEach(word => {
+            this.insert(word)
+        })
         if (!this.rawNumberString || !this.rawNumberString.length) return [];
 
         this._letterMatrix = this.rawNumberString.split("").map(digit => this._numberLetterMappings[digit] || [""]);
@@ -25,14 +40,52 @@ class TransformStringLogic {
         return this._letterMatrix.reduce((strings, currentEntry) => {
             const combinations = [];
             strings.reduce((_, current) => {
-                [...currentEntry].map(digit => combinations.push(`${current}${digit}`));
-            });
+                [...currentEntry].map((digit) => {
+                    if (this.isWord(`${current}${digit}`)) {
+                        combinations.push(`${current}${digit}`)
+                    }
+                });
+            }, "");
 
             return combinations;
         });
     }
+
+
+    insert(word) {
+        if (word.length === 0) return
+
+        let letter, currentNode = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            letter = word[i];
+
+            if (!currentNode.children.hasOwnProperty(letter)) {
+                currentNode.children[letter] = new TrieNode();
+            }
+            currentNode = currentNode.children[letter]
+        }
+        currentNode.isEnd = true;
+    }
+
+    _getNodeForPrefix(string) {
+        let letter, currentNode = this.root;
+
+        for (let i = 0; i < string.length; i++) {
+            letter = string[i];
+            if (!currentNode.children.hasOwnProperty(letter)) return null;
+
+            currentNode = currentNode.children[letter];
+        }
+        return currentNode
+    }
+
+    isWord(word) {
+        if (this._getNodeForPrefix(word) === null) return false;
+        return this._getNodeForPrefix(word).isEnd
+    }
+
 }
 
-const newWordConvert = new TransformStringLogic("237");
-
+const newWordConvert = new TrieTransformStringLogic("526");
 console.log(newWordConvert.letterCombination())
